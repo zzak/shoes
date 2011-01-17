84e933303e5b0ad6efdcb7b3c39b950d97b2a6c9
Zachary Scott <zachary.s.scott@gmail.com>
2011-01-17 03:16:04 -0500

    removing vlc support from common rake

--- make/rakefile_common.rb
+++ make/rakefile_common.rb
@@ -1,8 +1,6 @@
 task :build_os => [:buildenv_linux, :build_skel, "dist/#{NAME}"]
 
 task :buildenv_linux do
-  unless ENV['VIDEO']
-    rm_rf "dist"
-    mkdir_p "dist"
-  end
+  rm_rf "dist"
+  mkdir_p "dist"
 end
